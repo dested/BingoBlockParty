befbaa50f4cf28d25e4965d0affb73fb7c16b272
Salvatore <dested@gmail.com>
2014-03-01 13:11:00 -0700

    initial multiplatform commit

---
+++ Client.Web/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Web
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("hi");
+        }
+    }
+}
